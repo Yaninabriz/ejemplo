b8444da437353da3b04023c9cbf971edf0aa7ee5
yaninabriz <yanrodriguezbriz@gmnail.com>
2025-05-21 22:18:05 -0300

    mi primer componente

---
+++ components/banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <h1 className='bg-indigo-400'>hola</h1>
+      <p>Mi primer componente</p>
+    </div>
+  )
+}
+
+export default Banner
